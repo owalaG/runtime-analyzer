06f4566e10f0fa4591102c90df007312162204eb
George Owala <owalageorge@gmail.com>
2020-03-24 21:42:13 -0400

    added request for user input request

--- analyzer.py
+++ analyzer.py
@@ -7,4 +7,8 @@ def create_random_list(size, max_val):
         ran_list.append(random.randint(1,max_val))
     return ran_list
 
-print(create_random_list(40,80))
+size = int(input("What size of list do u want to create? "))
+max = int(input("What is the max value of the range? "))
+print(type(size), type(max))
+
+print(create_random_list(size, max))
