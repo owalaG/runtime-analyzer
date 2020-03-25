0db14385d23cdc1e9a00d6977a67b738ad5a4eba
George Owala <owalageorge@gmail.com>
2020-03-24 21:17:54 -0400

    created random list generator function

---
+++ analyzer.py
@@ -0,0 +1,10 @@
+import random
+import demos
+
+def create_random_list(size, max_val):
+    ran_list=[]
+    for num in range(size):
+        ran_list.append(random.randint(1,max_val))
+    return ran_list
+
+print(create_random_list(40,80))
