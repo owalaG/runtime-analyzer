38bbceabbcd0fb84b41026f76b8b27d44950c097
George Owala <owalageorge@gmail.com>
2020-03-25 02:08:13 -0400

    Added functions and clean the code

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,6 @@
 import time
 import random
-from demos import quicksort, mergesort
+from demos import quicksort, mergesort, bubblesort
 
 def create_random_list(size, max_val):
     ran_list=[]
@@ -8,15 +8,19 @@ def create_random_list(size, max_val):
         ran_list.append(random.randint(1,max_val))
     return ran_list
 
+
+def analyze_func(func_name, arr):
+    tic = time.time()
+    func_name(arr)
+    toc = time.time()
+    seconds = toc - tic
+    print("{}\t -> elasped time: {} seconds" .format(func_name.__name__.capitalize(), seconds))
+
+
 size = int(input("What size of list do u want to create? "))
 max = int(input("What is the max value of the range? "))
 
 l = create_random_list(size,max)
-tic = time.time()
-quicksort(l)
-toc = time.time()
-print("Quicksort elasped time --> ", toc-tic)
-tic = time.time()
-mergesort(l)
-toc = time.time()
-print("Mergesort elasped time --> ", toc-tic)
+analyze_func(bubblesort,l.copy()) #use copy to avoid sorted array being parsed 
+analyze_func(quicksort,l)
+analyze_func(mergesort,l)
